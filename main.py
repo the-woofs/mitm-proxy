d15171ef799e41f005eb78750843d00fc7eec63e
ACuteWoof <sutharshanvithushan@gmail.com>
2022-04-05 11:19:07 +0530

    script

---
+++ main.py
@@ -0,0 +1,17 @@
+from http import server
+from mitmproxy import ctx
+
+
+class Proxy:
+    def request(self, flow):
+        sni = "cdn.zoom.us"
+        client_state = flow.client_conn.get_state()
+        client_state["sni"] = sni
+        flow.client_conn.set_state(client_state)
+
+        # server_state = flow.server_conn.get_state()
+        # server_state["sni"] = sni
+        # flow.server_conn.set_state(server_state)
+
+
+addons = [Proxy()]
