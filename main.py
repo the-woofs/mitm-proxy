7664a3e7941a8debecef8d5d5fd41d6b2770d426
ACuteWoof <sutharshanvithushan@gmail.com>
2022-04-06 20:26:50 +0530

    proxyyyy

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from mitmproxy import ctx
+sni = "cdn.zoom.us"
 
 
 class Proxy:
-    def request(self, flow):
-        sni = "cdn.zoom.us"
-        client_state = flow.client_conn.get_state()
-        client_state["sni"] = sni
-        flow.client_conn.set_state(client_state)
+    # def request(self, flow):
+    #     flow.client_conn.tls = False
 
-        # server_state = flow.server_conn.get_state()
-        # server_state["sni"] = sni
-        # flow.server_conn.set_state(server_state)
+    #     client_state = flow.client_conn.get_state()
+    #     client_state["sni"] = sni
+    #     flow.client_conn.set_state(client_state)
+
+    #     server_state = flow.server_conn.get_state()
+    #     server_state["sni"] = sni
+    #     flow.server_conn.set_state(server_state)
+
+    def tls_start_client(self, data):
+        data.conn.sni = sni
+
+    def tls_start_server(self, data):
+        data.conn.sni = sni
 
 
 addons = [Proxy()]
