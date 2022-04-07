3cc0b232b1442cd11e2526de082e8500d6ac81e9
ACuteWoof <sutharshanvithushan@gmail.com>
2022-04-07 15:09:27 +0530

    rm unwanted comments

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ sni = "cdn.zoom.us"
 
 
 class Proxy:
-    # def request(self, flow):
-    #     flow.client_conn.tls = False
-
-    #     client_state = flow.client_conn.get_state()
-    #     client_state["sni"] = sni
-    #     flow.client_conn.set_state(client_state)
-
-    #     server_state = flow.server_conn.get_state()
-    #     server_state["sni"] = sni
-    #     flow.server_conn.set_state(server_state)
-
     def tls_start_client(self, data):
         data.conn.sni = sni
 
